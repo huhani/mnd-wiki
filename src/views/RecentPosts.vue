8db11adaa10c2d914baa231ba847f78f5d3220e7
Junyoung Cho <pom0894@naver.com>
2018-10-26 09:18:21 +0900

    최근 변경 항목 링크 적용

--- src/views/RecentPosts.vue
+++ src/views/RecentPosts.vue
@@ -1,11 +1,11 @@
 <template>
   <el-card class="box-card">
     <div slot="header" class="clearfix">
-      <span>최근 변경</span>
+      <span><b>최근 변경</b></span>
       <!--<el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button>-->
     </div>
     <div v-for="item in items" class="text item">
-      {{'List item ' + item }}
+      <a v-bind:href="'/w/' + item.title">{{item.title}}</a>
     </div>
   </el-card>
 
@@ -20,15 +20,26 @@
         data() {
           return {
           items: [
-            {item: 'Foo'},
-            {item: 'Bar'}
+            {title: 'mnd'},
+            {title: 'mndcert'},
+            {title: 'PRC-999K'},
+            {title: '국방위키(가칭)：대문'},
+            {title: '야전교범'}
             ]
           }
-        }
+        },
+      methods: {
+          test: function() {
+              this.$router.replace({ name: "read-document", params: {id:"123"}, query: {q1: "q1"} })
+          }
+      }
     }
 </script>
 
 <style scoped>
+  a {
+    text-decoration: none;
+  }
   .text {
     font-size: 14px;
   }
